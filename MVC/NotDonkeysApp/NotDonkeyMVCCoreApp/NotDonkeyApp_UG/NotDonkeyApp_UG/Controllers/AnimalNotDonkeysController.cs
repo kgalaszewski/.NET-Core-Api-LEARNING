77d1034693b79f605c6e5ad6f821fcaab2ef21e6
Karol Galaszewski <kgalaszewski95@gmail.com>
2019-05-17 13:54:04 +0200

    improved TODO, Added NotDonkey main app

---
+++ MVC/NotDonkeysApp/NotDonkeyMVCCoreApp/NotDonkeyApp_UG/NotDonkeyApp_UG/Controllers/AnimalNotDonkeysController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GraniteHouse.Data;
+using NotDonkeyApp_UG.Models;
+using NotDonkeyApp_UG.Services;
+using NotDonkeyApp_UG.Data;
+
+namespace NotDonkeyApp_UG.Controllers
+{
+    public class AnimalNotDonkeysController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AnimalNotDonkeysController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        #region Controller Basic Methods
+        public IActionResult Index()
+        {
+            try
+            {
+                var listOfAllUsers = _db.NotDonkeys.ToList();
+
+                return View(listOfAllUsers);
+            }
+            catch (Exception ex)
+            {
+                SetErrorDetails(ex, "An error occured during trying to reach main page");
+                return RedirectToAction("ThrowErrorMessage");
+            }
+        }
+
+        public IActionResult AllAnimals()
+        {
+            try
+            {
+                if (!StaticDetails.DonkeysAvailableToLike.Any())
+                {
+                    var listOfAllUsers = _db.NotDonkeys.ToList();
+
+                    StaticDetails.DonkeysAvailableToLike = listOfAllUsers;
+
+                    return View(listOfAllUsers);
+                }
+                else
+                {
+                    return View(StaticDetails.DonkeysAvailableToLike);
+                }
+            }
+            catch (Exception ex)
+            {
+                SetErrorDetails(ex, "An error occured during trying to display all animals");
+                return RedirectToAction("ThrowErrorMessage");
+            }
+        }
+
+        public IActionResult AddAnimalToFavourites(int id)
+        {
+            try
+            {
+                var user = _db.NotDonkeys.Find(StaticDetails.CurrentUserId);
+                if (user != null)
+                {
+                    user.AnimalsYouLike += $"{id.ToString()}.";
+                    StaticDetails.DonkeysAvailableToLike.Remove(StaticDetails.DonkeysAvailableToLike.Where(x => x.Id == id).SingleOrDefault());
+                    _db.SaveChanges();
+                    StaticDetails.DonkeysAvailableToLike = _db.NotDonkeys.ToList();
+                }
+                return RedirectToAction("AllAnimals");
+            }
+            catch (Exception ex)
+            {
+                SetErrorDetails(ex, "An error occured during trying to Add animal to your favourites");
+                return RedirectToAction("ThrowErrorMessage");
+            }
+        }
+
+        public IActionResult PairedAnimals()
+        {
+            try
+            {
+                var user = _db.NotDonkeys.Find(StaticDetails.CurrentUserId);
+                if (user != null)
+                {
+                    var allAnimalLikedids = AnimalService.Instance.ProceedUserIds(user.AnimalsYouLike ?? String.Empty);
+                    List<AnimalNotDonkey> yourAnimalsList = new List<AnimalNotDonkey>();
+
+                    foreach (var id in allAnimalLikedids)
+                    {
+                        yourAnimalsList.Add(_db.NotDonkeys.Find(id));
+                    }
+
+                    return View(yourAnimalsList);
+                }
+
+                return View(new List<AnimalNotDonkey>());
+            }
+            catch (Exception ex)
+            {
+                SetErrorDetails(ex, "An error occured during trying to get all paired animals");
+                return RedirectToAction("ThrowErrorMessage");
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(int ownerPhoneNumber, string password)
+        {
+            try
+            {
+                var user = _db.NotDonkeys.FirstOrDefault(x => x.OwnerPhoneNumber == ownerPhoneNumber);
+
+                if (user != null)
+                {
+                    if (user.Password == password)
+                    {
+                        StaticDetails.CurrentUserId = user.Id;
+
+                        return RedirectToAction("PairedAnimals");
+                    }
+                }
+                StaticDetails.IsUserLogged = true;
+                return View();
+            }
+            catch (Exception ex)
+            {
+                SetErrorDetails(ex, "An error occured during Logging in");
+                return RedirectToAction("ThrowErrorMessage");
+            }
+        }
+
+        public IActionResult Create()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                SetErrorDetails(ex, "An error occured");
+                return RedirectToAction("ThrowErrorMessage");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,OwnerPhoneNumber,Name,Password,AnimalType,Sex,Weight,AdditionalDescription,IsDonkey,AnimalsYouLike")] AnimalNotDonkey animalNotDonkey)
+        {
+            try
+            {
+                if (ModelState.IsValid && !animalNotDonkey.IsDonkey)
+                {
+                    animalNotDonkey.AnimalType = AnimalService.Instance.SetAnimalType(animalNotDonkey.AnimalType);
+                    _db.Add(animalNotDonkey);
+                    await _db.SaveChangesAsync();
+                    StaticDetails.DonkeysAvailableToLike = _db.NotDonkeys.ToList();
+                    return View("Login");
+                }
+                return View(animalNotDonkey);
+            }
+            catch (Exception ex)
+            {
+                SetErrorDetails(ex, "Error occured during creating new animal");
+                return RedirectToAction("ThrowErrorMessage");
+            }
+        }
+
+        public IActionResult ThrowErrorMessage()
+        {
+            return View();
+        }
+
+        public IActionResult UserProfile()
+        {
+            if (StaticDetails.CurrentUserId > 0)
+            {
+                var currentUser = _db.NotDonkeys.Find(StaticDetails.CurrentUserId);
+                return View(currentUser);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+        private void SetErrorDetails(Exception ex, string msgToDisplay)
+        {
+            StaticDetails.CurrentErrorMsg = $"An error occured due to : {ex.Message}";
+            StaticDetails.AdditionalErrorInfo = msgToDisplay;
+        }
+        #endregion
+    }
+}
---
+++ MVC/NotDonkeysApp/NotDonkeyMVCCoreApp/NotDonkeyApp_UG/NotDonkeyApp_UG/Data/StaticDetails.cs
@@ -0,0 +1,38 @@
+﻿using NotDonkeyApp_UG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotDonkeyApp_UG.Data
+{
+    
+
+ // @"\" + @"images\AnimalImages" + @"\" + ProductsVM.Products.Id + ".jpg";
+    public static class StaticDetails
+    {
+        /// <summary>
+        /// Currently logged user info
+        /// </summary>
+        public static int CurrentUserId { get; set; }
+
+        /// <summary>
+        /// AvatarUrl + AnimalType from AnimalNotDonkeyModel == path to img
+        /// </summary>
+        public static readonly string AvatarUrl = @"\images\AnimalImages\";
+        public static readonly string FullPathToErrorImg = @"\images\AnimalImages\ErrorImg.jpg";
+
+        /// <summary>
+        /// List will be edited after you like some animal
+        /// </summary>
+        public static List<AnimalNotDonkey> DonkeysAvailableToLike = new List<AnimalNotDonkey>();
+
+        /// <summary>
+        /// Erorr MSG and AdditionalErrorInfo will be assigned and used when TryCatchError method inside AnimalNotDonkeysController catches an error
+        /// </summary>
+        public static string CurrentErrorMsg { get; set; }
+        public static string AdditionalErrorInfo { get; set; }
+
+        public static bool IsUserLogged = false;
+    }
+}
