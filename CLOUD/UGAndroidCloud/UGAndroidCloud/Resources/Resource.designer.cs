b88fe586e745c2f3737c5659aedccc7ba59dcaaf
Karol Galaszewski <kgalaszewski95@gmail.com>
2019-09-09 16:12:52 +0200

    ?

--- CLOUD/UGAndroidCloud/UGAndroidCloud/Resources/Resource.designer.cs
+++ CLOUD/UGAndroidCloud/UGAndroidCloud/Resources/Resource.designer.cs
@@ -2327,9 +2327,6 @@ namespace UGAndroidCloud
 			// aapt resource value: 0x7f0a0032
 			public const int ALT = 2131361842;
 			
-			// aapt resource value: 0x7f0a0091
-			public const int AllInfo = 2131361937;
-			
 			// aapt resource value: 0x7f0a008c
 			public const int Breed = 2131361932;
 			
@@ -2488,6 +2485,9 @@ namespace UGAndroidCloud
 			
 			// aapt resource value: 0x7f0a006c
 			public const int customPanel = 2131361900;
+			
+			// aapt resource value: 0x7f0a0091
+			public const int dataRecyclerView = 2131361937;
 			
 			// aapt resource value: 0x7f0a007a
 			public const int decor_content_parent = 2131361914;
@@ -3098,19 +3098,22 @@ namespace UGAndroidCloud
 			public const int notification_template_part_time = 2130968631;
 			
 			// aapt resource value: 0x7f040038
-			public const int select_dialog_item_material = 2130968632;
+			public const int row = 2130968632;
 			
 			// aapt resource value: 0x7f040039
-			public const int select_dialog_multichoice_material = 2130968633;
+			public const int select_dialog_item_material = 2130968633;
 			
 			// aapt resource value: 0x7f04003a
-			public const int select_dialog_singlechoice_material = 2130968634;
+			public const int select_dialog_multichoice_material = 2130968634;
 			
 			// aapt resource value: 0x7f04003b
-			public const int support_simple_spinner_dropdown_item = 2130968635;
+			public const int select_dialog_singlechoice_material = 2130968635;
 			
 			// aapt resource value: 0x7f04003c
-			public const int tooltip = 2130968636;
+			public const int support_simple_spinner_dropdown_item = 2130968636;
+			
+			// aapt resource value: 0x7f04003d
+			public const int tooltip = 2130968637;
 			
 			static Layout()
 			{
